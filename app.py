46f98dbcfa73c31b99234ed3c8f27f1819e2d96c
X-Mohsen <MaybeEvmo@gmail.com>
2025-04-25 02:16:55 +0330

    app.py : Remove auth + PEP8

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template, send_file, request
-import flask_login
 from werkzeug.utils import secure_filename
 
 from utils import network, storage
@@ -11,95 +10,32 @@ ip_addr = network.get_local_ipv4()
 port = network.port
 
 app = Flask(__name__)
-login_manager = flask_login.LoginManager()
 app.secret_key = os.urandom(24)
-login_manager.init_app(app)
-
-class User(flask_login.UserMixin):
-    pass
-
-users = {'admin': {'password': 'admin'}}
-
-
-@login_manager.user_loader
-def user_loader(email):
-    if email not in users:
-        return
-
-    user = User()
-    user.id = email
-    return user
-
-
-@login_manager.request_loader
-def request_loader(request):
-    email = request.form.get('email')
-    if email not in users:
-        return
-
-    user = User()
-    user.id = email
-    return user
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return render_template("login.html")
-
-    email = request.form['email']
-    if email in users and request.form['password'] == users[email]['password']:
-        user = User()
-        user.id = email
-        flask_login.login_user(user)
-        return render_template('upload.html', logged_in = True, ip_addr=ip_addr,port=port, storage_dir_name=storage_dir_name)
-
-    return render_template("login.html", error = "Invalid Data !")
-
-
-@app.route('/protected')
-@flask_login.login_required
-def protected():
-    return render_template("index_files.html")
-
-@app.route('/logout')
-def logout():
-    flask_login.logout_user()
-    return render_template("upload.html", ip_addr=ip_addr,port=port, message = "Logged out.")
-
-@login_manager.unauthorized_handler
-def unauthorized_handler():
-    return render_template("unauth.html")
 
 
 @app.route("/")
 @app.route("/up")
 def upload_func():
-    if flask_login.current_user.is_authenticated == False:
-        return render_template("upload.html", ip_addr=ip_addr,port=port, files_dir_name=storage_dir_name)
-    else:
-        return render_template("upload.html", ip_addr=ip_addr,port=port, logged_in = True)
+    return render_template("upload.html", ip_addr=ip_addr, port=port, files_dir_name=storage_dir_name)
 
 
-
-@app.route("/uploader", methods = ["GET","POST"])
+@app.route("/uploader", methods=["GET", "POST"])
 def uploader():
     if request.method == "POST":
 
         if 'file' not in request.files:
-            return render_template("upload.html", message="No selected file", ip_addr=ip_addr,port=port)
+            return render_template("upload.html", message="No selected file", ip_addr=ip_addr, port=port)
 
         file = request.files["file"]
-        
+
         if file.filename == "":
-            return render_template("upload.html", message="No selected file", ip_addr=ip_addr,port=port)
-        
+            return render_template("upload.html", message="No selected file", ip_addr=ip_addr, port=port)
+
         if file:
             try:
                 print("Started saving file ... ")
 
                 # Use a streaming approach to save the file in chunks
-                
                 file_path = os.path.join(storage_path, file.filename)
                 with open(file_path, "wb") as f:
                     while True:
@@ -111,16 +47,16 @@ def uploader():
                 print("File saved successfully.")
 
             except Exception as e:
-                return render_template("upload.html", message=f"Error: {str(e)}", ip_addr=ip_addr,port=port)
+                return render_template("upload.html", message=f"Error: {str(e)}", ip_addr=ip_addr, port=port)
 
-    return render_template("up_done.html", ip_addr=ip_addr,port=port)
+    return render_template("up_done.html", ip_addr=ip_addr, port=port)
 
 
 @app.route("/<path:req_path>")
 def index_files_func(req_path):
     base_dir = "./"
     abs_path = os.path.join(base_dir, req_path)
-    
+
     if not os.path.exists(abs_path):
         return f"{abs_path}"
     if os.path.isfile(abs_path):
